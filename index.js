e0a07515145d1c3902cc02e4e22a42322a09131c
Luis Argumedo <huicho.argumedo@gmail.com>
2015-11-22 00:01:29 -0600

    Change Port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello Beacon!');
 });
 
-var server = app.listen(6666, () => {
+var server = app.listen(3000, () => {
   var host = server.address().address;
   var port = server.address().port;
 
